b97706b8de72ffa6d6d74cf712434ed9fe41b5fb
Moisés Rocha <moisesrocha99@gmail.com>
2021-04-24 15:37:27 +0100

    Switch neovim plugin manager to packer.nvim

---
+++ packer.nvim.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Installing packer.nvim plugin manager for neovim
+echo "Installing packer.nvim"
+git clone https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
